59bad1f94487915e934d80041fd75c5d6f01e499
San4oyzik <san4oyzik@gmail.com>
2025-06-16 02:04:34 +0500

    fixasd

--- db_connect.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-const url = process.env.DB_CONNECTION_URL
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
-
-module.exports = mongoose
